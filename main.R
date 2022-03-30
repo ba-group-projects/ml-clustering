05b5ac5f2ddf2da93fc039a01438358bbff7f664
mattlim96-Desktop <mattlim96@gmail.com>
2022-03-30 12:59:57 +0100

    Complete preprocess and correlation matrix

--- main.R
+++ main.R
@@ -1,4 +1,33 @@
-library(GGally)
-library(dplyr)
 
+# import libraries
+library(dplyr)
+library(GGally)
+library(corrplot)
+
+# import data
 data.ori = read.csv("customer-personality.csv")
+
+# summary of data
+str(data.ori)
+
+# check for NA
+sum(is.na(data.ori))
+
+# check which columns contain NA
+apply(is.na(data.ori), 2, which)
+
+# remove NA rows
+data.clean = data.ori[which(!is.na(data.ori$Income)),]
+str(data.clean)
+sum(is.na(data.clean))
+
+# filter numerical columns
+numerical.data = data.clean[, c(2,5:7,9:ncol(data.ori))]
+str(numerical.data)
+
+# plot correlation matrix
+corrplot(cor(numerical.data), tl.col = "black", diag = FALSE, 
+         method = 'number', type = 'upper')
+
+# # inefficient to use this, too much data
+# ggpairs(numerical.data)#, aes(color = class, alpha = 0.5))
