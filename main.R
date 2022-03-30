e283caa776a3cecc279b4b2a29851daeb577d8f4
nickakara <91995206+nickakara@users.noreply.github.com>
2022-03-30 14:59:32 +0100

    updated with FA

--- main.R
+++ main.R
@@ -55,3 +55,68 @@ data.clean$Marital_Status[data.clean$Marital_Status == "Divorced"] <- "4"
 data.clean$Marital_Status[data.clean$Marital_Status == "Widow"] <- "5"
 data.clean$Marital_Status <- as.numeric(data.clean$Marital_Status)
 
+
+
+######## Factor Analysis ########
+
+### determine the number of factors of FA
+# We want to identify the component of the correlation structure
+
+data.clean$Dt_Customer <- NULL
+
+
+fa.cor = cor(data.clean)
+
+
+# looking at fa.cor
+# let's look at eigenvector of the correlation to see how many factors we use
+
+fa.eigen = eigen(fa.cor)
+fa.eigen$values
+# in decreasing values
+# higher = more important
+# rule of thuumb to see the number of variables we should use? Do the sum
+
+sum(fa.eigen$values)
+
+
+cumsum(fa.eigen$values)/19
+# seeing this, using the first 4, we can explain 79% of the data
+# draw screen plot for better visualisation
+
+
+# use the scree plot
+plot(fa.eigen$values, type = "b", ylab = "Eigenvalues", xlab = "Factor") # we choose 4
+plot(cumsum(fa.eigen$values)/17, type = "b", ylab = "Eigenvalues", xlab = "Factor") # we choose 4
+
+# maybe use 7?
+# Why we use 4
+# Because after 4, the decrease in the eigenvalue is much smaller, therefore we choose 4
+
+##############################################
+### factor analysis
+fa.res = factanal(x = data.clean, factors = 7, rotation = "none") # factor = 4 because of our eigenvalues easlier
+fa.res
+
+#uniqueness = epsilon values
+# loadings
+# We have proportion variance explained by these loadings
+# hypothesis test to see whether 4 factors are sufficient
+# here, our p value is high therefore we don't reject that 4 factors are sufficient
+
+
+##############################################
+### factor rotation
+fa.res = factanal(x = data.clean, factors = 4, rotation = "promax")
+# promax belongs to the oblique rotation?
+
+print(fa.res, cut = 0.2)
+# we only show the factors with a loading of higher than 0.2
+# therefore only selecting the variables with large loading
+# you can see that paragraph, sentence and wordm is with factor 1
+##############################################
+### factor scores
+fa.res = factanal(x = data.clean, factors = 7, rotation = "promax", scores = "Bartlett")
+head(fa.res$scores)
+summary(lm(Factor2 ~ Factor1, data = as.data.frame(fa.res$scores)))
+
